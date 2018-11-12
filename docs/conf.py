a13e0c14811b4e5781806ced6849360f5436706b
tscanlon <tracy.scanlon@geo.tuwien.ac.at>
2018-11-12 17:03:54 +0100

    removal of unicode text in conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -40,8 +40,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'c3s_sm'
-copyright = u'2018, Wolfgang Preimesberger'
+project = 'c3s_sm'
+copyright = '2018, Wolfgang Preimesberger'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
