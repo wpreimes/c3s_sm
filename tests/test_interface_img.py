b3772ef6a52fa71b04ed62c85f7e77bc01395af5
Wolfgang Preimesberger <wolfgang.preimesberger@geo.tuwien.ac.at>
2021-02-23 23:30:05 +0100

    Update image reading

--- tests/test_interface_img.py
+++ tests/test_interface_img.py
@@ -5,29 +5,27 @@ import os
 import numpy.testing as nptest
 from smecv_grid.grid import SMECV_Grid_v052
 
-def test_C33Ts_tcdr_combined_daily():
+def test_C3STs_tcdr_combined_daily():
     file = os.path.join(os.path.join(os.path.dirname(__file__),
                         'c3s_sm-test-data', 'img', 'TCDR', '060_dailyImages', 'combined', '2014',
                         'C3S-SOILMOISTURE-L3S-SSMV-COMBINED-DAILY-20140101000000-TCDR-v201801.0.0.nc'))
 
 
-    ds = C3SImg(file, mode='r', parameters='sm', array_1D=False)
+    ds = C3SImg(file, mode='r', parameters='sm', flatten=False)
     image= ds.read()
 
     nptest.assert_almost_equal(image.data['sm'][167, 785], 0.34659, 4)
     assert(image.metadata['sm']['long_name'] == 'Volumetric Soil Moisture')
-    #assert(file_meta['creator_name'] == 'Earth Observation Data Center (EODC)')
 
 
-
-def test_C33Ts_tcdr_active_monthly():
+def test_C3STs_tcdr_active_monthly():
 
     file = os.path.join(os.path.join(os.path.dirname(__file__),
                         'c3s_sm-test-data', 'img', 'TCDR', '061_monthlyImages', 'active',
                         'C3S-SOILMOISTURE-L3S-SSMS-ACTIVE-MONTHLY-20140101000000-TCDR-v201801.0.0.nc'))
 
 
-    ds = C3SImg(file, mode='r', parameters='sm', array_1D=False)
+    ds = C3SImg(file, mode='r', parameters='sm', flatten=False)
     image = ds.read()
 
     nptest.assert_almost_equal(image.data['sm'][167, 785], 47.69982, 4)
@@ -36,13 +34,13 @@ def test_C33Ts_tcdr_active_monthly():
     #assert(file_meta['creator_name'] == 'Earth Observation Data Center (EODC)')
 
 
-def test_C33Ts_tcdr_passive_decadal():
+def test_C3STs_tcdr_passive_decadal():
     file = os.path.join(os.path.join(os.path.dirname(__file__),
                         'c3s_sm-test-data', 'img', 'TCDR', '062_dekadalImages', 'passive',
                         'C3S-SOILMOISTURE-L3S-SSMV-PASSIVE-DEKADAL-20140101000000-TCDR-v201801.0.0.nc'))
 
 
-    ds = C3SImg(file, mode='r', parameters='sm', array_1D=False)
+    ds = C3SImg(file, mode='r', parameters='sm', flatten=False)
     image = ds.read()
 
     nptest.assert_almost_equal(image['sm'][167, 784], 0.50875, 4)
@@ -51,13 +49,13 @@ def test_C33Ts_tcdr_passive_decadal():
 
 ################################################################################
 
-def test_C33Ts_icdr_combined_daily():
+def test_C3STs_icdr_combined_daily():
     file = os.path.join(os.path.join(os.path.dirname(__file__),
                         'c3s_sm-test-data', 'img', 'ICDR', '060_dailyImages', 'combined', '2017',
                         'C3S-SOILMOISTURE-L3S-SSMV-COMBINED-DAILY-20170701000000-ICDR-v201706.0.0.nc'))
 
 
-    ds = C3SImg(file, mode='r', parameters='sm', array_1D=False)
+    ds = C3SImg(file, mode='r', parameters='sm', flatten=False)
     image = ds.read()
 
     nptest.assert_almost_equal(image.data['sm'][167, 785], 0.14548, 4)
@@ -66,14 +64,14 @@ def test_C33Ts_icdr_combined_daily():
 
 
 
-def test_C33Ts_icdr_active_monthly():
+def test_C3STs_icdr_active_monthly():
 
     file = os.path.join(os.path.join(os.path.dirname(__file__),
                         'c3s_sm-test-data', 'img', 'ICDR', '061_monthlyImages', 'active',
                         'C3S-SOILMOISTURE-L3S-SSMS-ACTIVE-MONTHLY-20170701000000-ICDR-v201706.0.0.nc'))
 
 
-    ds = C3SImg(file, mode='r', parameters='sm', array_1D=False)
+    ds = C3SImg(file, mode='r', parameters='sm', flatten=False)
     image = ds.read()
 
     nptest.assert_almost_equal(image.data['sm'][167, 785], 65.00162, 4)
@@ -82,13 +80,13 @@ def test_C33Ts_icdr_active_monthly():
     #assert(file_meta['creator_name'] == 'Earth Observation Data Center (EODC)')
 
 
-def test_C33Ts_icdr_passive_decadal():
+def test_C3STs_icdr_passive_decadal():
     file = os.path.join(os.path.join(os.path.dirname(__file__),
                         'c3s_sm-test-data', 'img', 'ICDR', '062_dekadalImages', 'passive',
                         'C3S-SOILMOISTURE-L3S-SSMV-PASSIVE-DEKADAL-20170701000000-ICDR-v201706.0.0.nc'))
 
 
-    ds = C3SImg(file, mode='r', parameters='sm', array_1D=False)
+    ds = C3SImg(file, mode='r', parameters='sm', flatten=False)
     image = ds.read()
 
     nptest.assert_almost_equal(image.data['sm'][167, 785], 0.21000, 4)
@@ -102,7 +100,7 @@ def test_1Dreading():
                         'c3s_sm-test-data', 'img', 'ICDR', '060_dailyImages', 'combined', '2017',
                         'C3S-SOILMOISTURE-L3S-SSMV-COMBINED-DAILY-20170701000000-ICDR-v201706.0.0.nc'))
 
-    ds = C3SImg(file, mode='r', parameters='sm', array_1D=True)
+    ds = C3SImg(file, mode='r', parameters='sm', flatten=True)
     image = ds.read()
 
     assert ds.grid.find_nearest_gpi(75.625, 14.625) == (602942, 0)
@@ -117,7 +115,7 @@ def test_1Dreading():
 
     land_grid = SMECV_Grid_v052('land')
 
-    ds = C3SImg(file, mode='r', parameters='sm', array_1D=True, subgrid=land_grid)
+    ds = C3SImg(file, mode='r', parameters='sm', flatten=True, grid=land_grid)
     image = ds.read()
 
     assert ds.grid.find_nearest_gpi(75.625, 14.625) == (602942, 0)
@@ -132,14 +130,11 @@ def test_1Dreading():
 
     assert(image.metadata['sm']['long_name'] == 'Volumetric Soil Moisture')
 
-
-
 if __name__ == '__main__':
     test_1Dreading()
-    test_C33Ts_tcdr_combined_daily()
-    test_C33Ts_tcdr_active_monthly()
-    test_C33Ts_tcdr_passive_decadal()
-
-    test_C33Ts_icdr_combined_daily()
-    test_C33Ts_icdr_active_monthly()
-    test_C33Ts_icdr_passive_decadal()
+    test_C3STs_icdr_active_monthly()
+    test_C3STs_icdr_combined_daily()
+    test_C3STs_icdr_passive_decadal()
+    test_C3STs_tcdr_active_monthly()
+    test_C3STs_tcdr_combined_daily()
+    test_C3STs_tcdr_passive_decadal()
